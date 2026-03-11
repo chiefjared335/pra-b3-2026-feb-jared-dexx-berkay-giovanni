d6bc6324f889f5f5ad1753c7f4aa69c9b168a610
chiefjared335 <jaredaertsschoolopdrachten@gmail.com>
2026-03-11 13:20:11 +0100

    gelinkt aan create en form create gemaakt

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 
 <head>
     <title></title>
-    <?php require_once 'head.php'; ?>
+    <?php require_once 'task/head.php'; ?>
+
 </head>
 
 <body>
     
     <div class="container">
-
+    <a href="task/create.php">Create Task</a>
 
     </div>
 
---
+++ task/create.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>maak een taak aan</h1>
+    <div class= "form-group":>
+    <label for="taak">taak:</label>
+    <input type="text" id="taak" name="taak" placeholder="naam van de taak"> <br>
+    <label for="beschrijving">beschrijving:</label>
+    <input type="text" id="beschrijving" name="beschrijving" placeholder="beschrijving van de taak"> <br>
+    <label for="afdeling">afdeling:</label>
+    <select name="afdeling" id="afdeling">
+    <option value="">– kies een afdeling –</option>
+    <option value="personeel">personeel</option>
+    <option value="horeca">horeca</option>
+    <option value="techniek">techniek</option>
+    <option value="inkoop">inkoop</option>
+    <option value="klantenservice">klantenservice</option>
+    <option value="groen">groen</option>
+    </select>
+    
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
